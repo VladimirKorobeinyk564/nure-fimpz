226b151301ef228270574bbaf46cefb61e60f36a
vladimirkorobeinyk <vovakorobeinyk2001@gmail.com>
2023-12-04 23:54:07 +0200

    Minor update

---
+++ src/components/common/section-divider/section-divider.tsx
@@ -0,0 +1,8 @@
+
+function SectionDivider() {
+    return (
+        <hr className={'border-[#ebecf0] dark:border-[#343a46] my-[40px]'}/>
+    );
+}
+
+export default SectionDivider;
--- src/components/pages/docs/content/pz/pz-1/first-practical-work.tsx
+++ src/components/pages/docs/content/pz/pz-1/first-practical-work.tsx
@@ -1,8 +1,12 @@
+import SectionDivider from "@/components/common/section-divider/section-divider.tsx";
 
 function FirstPracticalWork() {
     return (
         <div className={'w-full'}>
             <p>First practical work</p>
+            <SectionDivider></SectionDivider>
+            <p>First practical work</p>
+
         </div>
     );
 }
