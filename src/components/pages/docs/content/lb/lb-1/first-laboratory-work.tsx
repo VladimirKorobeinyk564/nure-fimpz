b9defcaad6cf6c67ee8ee2983f160884272f7ecd
vladimirkorobeinyk <vovakorobeinyk2001@gmail.com>
2023-12-05 12:15:46 +0200

    Minor update

--- src/components/pages/docs/content/lb/lb-1/first-laboratory-work.tsx
+++ src/components/pages/docs/content/lb/lb-1/first-laboratory-work.tsx
@@ -1,13 +1,10 @@
 import SectionDivider from "@/components/common/section-divider/section-divider.tsx";
 import ContractProgramming from "@/components/pages/docs/content/lb/lb-1/contract-programming/contract-programming.tsx";
-import TheoreticalInformation
-    from "@/components/pages/docs/content/lb/lb-1/theoretical-information/theoretical-information.tsx";
+import TheoreticalInformation from "@/components/pages/docs/content/lb/lb-1/theoretical-information/theoretical-information.tsx";
 import UsingOpenjml from "@/components/pages/docs/content/lb/lb-1/using-openjml/using-openjml.tsx";
-import TestingCustomizedUtility
-    from "@/components/pages/docs/content/lb/lb-1/testing-customized-utility/testing-customized-utility.tsx";
+import TestingCustomizedUtility from "@/components/pages/docs/content/lb/lb-1/testing-customized-utility/testing-customized-utility.tsx";
 import ProblemStatement from "@/components/pages/docs/content/lb/lb-1/problem-statement/problem-statement.tsx";
-import VariantsIndividualTasks
-    from "@/components/pages/docs/content/lb/lb-1/variants-individual-tasks/variants-individual-tasks.tsx";
+import VariantsIndividualTasks from "@/components/pages/docs/content/lb/lb-1/variants-individual-tasks/variants-individual-tasks.tsx";
 import QuestionsAndTasks from "@/components/pages/docs/content/lb/lb-1/questions-and-tasks/questions-and-tasks.tsx";
 import ReferencesList from "@/components/pages/docs/content/lb/lb-1/references-list/references-list.tsx";
 
