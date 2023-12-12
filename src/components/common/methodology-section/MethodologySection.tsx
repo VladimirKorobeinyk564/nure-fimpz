c852fa5b160cf1e1966706e1cf3882f9d75e0cc8
vladimirkorobeinyk <vovakorobeinyk2001@gmail.com>
2023-12-12 19:08:50 +0200

    Add Toast UI component and expand menu tree functionality
    A Toast UI component has been implemented which includes a new toast icon file, Toast UI elements and related state management logic. Also, added an action to expand menu items in the menu tree state. The changes also include some dependencies updates in the package-lock.json file.

--- src/components/common/methodology-section/MethodologySection.tsx
+++ src/components/common/methodology-section/MethodologySection.tsx
@@ -1,4 +1,8 @@
 import React from "react";
+import {useLocation} from "react-router";
+
+import copyLink from "@/assets/icons/common/copy-link.svg";
+import {toast} from "@/components/ui/toast/use-toast.ts";
 
 interface MethodologySectionProps {
     title: string;
@@ -7,13 +11,45 @@ interface MethodologySectionProps {
     isFirstSection: boolean;
 }
 
-const MethodologySection = (props: MethodologySectionProps) => {
-    const {title, children, id, isFirstSection} = props;
+const MethodologySection = (props: Readonly<MethodologySectionProps>) => {
+    const { title, children, id, isFirstSection } = props;
+    const location = useLocation();
+
+    function copyLinkToClipboard(): void {
+        let currentURL = window.location.origin + location.pathname;
+
+        if (!currentURL.includes(`#`)) currentURL += `#${id}`;
+
+        navigator.clipboard.writeText(currentURL)
+            .then(() => {
+                toast({
+                    duration: 700,
+                    description: "Link to article copied successfully.",
+                })
+            })
+            .catch((err) => {
+                console.error('Failed to copy URL to clipboard:', err);
+                toast({
+                    variant: "destructive",
+                    duration: 700,
+                    description: "Article link copying error.",
+                })
+            });
+    }
+
     return (
         <div id={id}>
-            {isFirstSection ?
-                <h3 className={"text-[40px] font-bold mb-[30px]"}>{title}</h3>
-                : <h4 className={"text-[30px] font-bold mb-[30px]"}>{title}</h4>}
+            <div className="flex mb-[30px] items-center">
+                {isFirstSection ?
+                    <h3 className={"text-[40px] font-bold"}>{title}</h3>
+                    : <h4 className={"text-[30px] font-bold"}>{title}</h4>}
+                <button
+                    className={'ml-[14px] w-[30px] p-[6px] h-[30px] cursor-pointer rounded-[6px] hover:bg-[rgba(64,71,86,0.1)]'}
+                    onClick={copyLinkToClipboard}
+                >
+                    <img src={copyLink} alt="Copy link"/>
+                </button>
+            </div>
             <div className={"text-[18px] leading-[36px]"}>
                 {children}
             </div>
